9286d5abd2fa8d7f8101ab64d8e7d490553477b2
David Cole <david.cole@digitalcharleston.com>
2013-11-23 09:44:11 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SkaMusic::Application.config.secret_key_base = 'd85a1e0437249f2c3cea52fb76d6caaa0087fb818916ee71e82af92c30ad4e054152e9b321b7ac4f5fba00dc214f98d723bc324a034d26fdb764b3b0ecad294d'
