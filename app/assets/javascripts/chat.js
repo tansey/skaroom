6da144bb4a3b6997793088edbdb7e705f8afdb31
David Cole <david.cole@digitalcharleston.com>
2013-11-27 10:59:54 -0500

    Queue setup. Websockets to www.skamusicroom.com.

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -1,7 +1,8 @@
 
 $( document ).ready( function() {
 
-  var dispatcher = new WebSocketRails( 'skamusic.herokuapp.com/websocket' );
+  var dispatcher = new WebSocketRails( 'www.skamusic.com/websocket' );
+  //var dispatcher = new WebSocketRails( 'localhost:3000/websocket' );
 
   dispatcher.bind( 'who_is_connected', function( who ) {
     for ( var i in who.rudies ) {
@@ -44,4 +45,25 @@ $( document ).ready( function() {
       scrollTop: $('#chat_text')[0].scrollHeight
     }, 800);
   } );
+
+  $( "#chat_tab a" ).click( function( event ) {
+    event.preventDefault();
+    if ( ! $( "#chat_tab" ).hasClass( 'active' ) ) {
+      $( "#chat_tab" ).addClass( 'active' );
+      $( "#queue_tab" ).removeClass( 'active' );
+      $( "#chat_panel" ).show();
+      $( "#queue_panel" ).hide();
+    }
+  } );
+
+  $( "#queue_tab a" ).click( function( event ) {
+    event.preventDefault();
+    if ( ! $( "#queue_tab" ).hasClass( 'active' ) ) {
+      $( "#queue_tab" ).addClass( 'active' );
+      $( "#chat_tab" ).removeClass( 'active' );
+      $( "#queue_panel" ).show();
+      $( "#chat_panel" ).hide();
+    }
+  } );
+
 } );
