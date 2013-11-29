4a3d7eb5768cf085720a3038442f7d4a87a50bcb
David Cole <david.cole@digitalcharleston.com>
2013-11-29 17:11:35 -0500

    puts.

--- app/controllers/queue_controller.rb
+++ app/controllers/queue_controller.rb
@@ -1,6 +1,6 @@
 class QueueController < ApplicationController
   def upload
-    put "PARAMS: #{ params.inspect }"
+    puts "PARAMS: #{ params.inspect }"
     render json: { files: [ { title: "Pressure Drop", artist: "Toots and the Maytails" }, { title: "Big Guns Down", artist: "Easy Big Fella" } ] }
   end
 end
