c51acc75137ed1b9da1146aaa341ea8a20dc7f7d
David Cole <david.cole@digitalcharleston.com>
2013-11-29 17:34:57 -0500

    Content type.

--- app/controllers/queue_controller.rb
+++ app/controllers/queue_controller.rb
@@ -1,6 +1,6 @@
 class QueueController < ApplicationController
   def upload
-    puts "PARAMS: #{ params[ :upload_music ].inspect }"
+    puts "PARAMS: #{ params[ :upload_music ].content_type.inspect }"
     # song = Song.new params[ :upload_music ]
     render json: { files: [ { title: "Pressure Drop", artist: "Toots and the Maytails" }, { title: "Big Guns Down", artist: "Easy Big Fella" } ] }
   end
